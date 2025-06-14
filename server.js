dcfc1d110174be00618977b668c9046e7dff481d
Nicole Gales <nicolegales@hotmail.com>
2025-06-14 18:39:17 -0400

    Fix path to index.html for deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // ✅ Serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './public/index.html'));
 });
 
 // === MongoDB schema + model

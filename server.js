a6acc82c76f2277f2fcf06002c27073d20e3221c
Nicole Gales <nicolegales@hotmail.com>
2025-06-14 16:24:20 -0400

    Fix public path to remove src reference

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.json());
 
 // ✅ Serve static files from /public
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // === MongoDB schema + model
